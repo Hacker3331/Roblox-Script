c94f928741756cd85059a93425a7e16d731042a7
EAS Studios <150769898+EASStudio81@users.noreply.github.com>
2024-01-19 19:09:08 +0700

    GiverBadgeScript.lua

--- GiverBadgeScript.lua
+++ GiverBadgeScript.lua
@@ -1,10 +1,9 @@
 local badgeID == "ID BADGE"
-    local badgeService == game:GetService("BadgeService")
+local badgeService = game:GetService("BadgeService")
 
 game.Players.PlayerAdded:Connect(function(player)
     wait(1)
-    if not badgeService:UserHasBadge(player.UserID, badgeID) then
-      badgeService:AwardBadge(player.UserID, badgeID)
+    if not badgeService:UserHasBadge(player.userID, badgeID) then
+        badgeService:AwardBadge(player.UserID, badgeID)
     end
-  end
 end)
